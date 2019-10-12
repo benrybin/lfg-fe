d412c7c46f02e82cd0855691b6251dc3fd3a794d
Ben Rybinski <45921913+benrybin@users.noreply.github.com>
2019-10-12 17:17:24 -0400

    added class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersignupComponent } from './usersignup/usersignup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersignupComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/user.ts
@@ -0,0 +1,14 @@
+export class User {
+ userName: String;
+ pw: String;
+ emailAddress:String;
+ profilePic:String;
+ banner:String;
+ intro: String;
+ timeZone: String;
+ region: String;
+ language: String;
+ mic: Boolean;
+ discordName: String;
+ steamName: String;    
+}
