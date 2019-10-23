acfaf1d814ca69714efa36786a9246009d0087cb
Ben Rybinski <45921913+benrybin@users.noreply.github.com>
2019-10-23 16:45:26 -0400

    valids

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersignupComponent } from './usersignup/usersignup.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersignupComponent
+    UsersignupComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/usersignup/usersignup.component.ts
+++ src/app/usersignup/usersignup.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-usersignup',
   templateUrl: './usersignup.component.html',
   styleUrls: ['./usersignup.component.css']
 })
 export class UsersignupComponent implements OnInit {
-
-  constructor() { }
+  
+  userSignupForm =this.fb.group({
+    userName: ['',Validators.required,Validators.minLength(5),],
+    pw:['',Validators.required,Validators.minLength(6)],
+    emailAddress:['',Validators.required]
+  })
+  
+  constructor(private fb:FormBuilder) {
+   
+   }
 
   ngOnInit() {
+    
+  }
+  onSubmit(){
+    console.log(this.userSignupForm.value);
   }
 
 }
